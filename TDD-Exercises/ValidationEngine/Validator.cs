91549fe7cbfa2b4c9f575fc83fccc891bd2b44a7
Therese Sjögren <nike_8@hotmail.com>
2017-05-22 13:08:50 +0200

    First TestMethod done

---
+++ TDD-Exercises/ValidationEngine/Validator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ValidationEngine
+{
+    public class Validator
+    {
+        private string validemail = @"^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$";
+
+
+        public bool ValidateEmailAddress(string v)
+        {
+            var resultMatch=Regex.IsMatch(v, validemail);
+            return resultMatch;
+        }
+    }
+}
---
+++ TDD-Exercises/ValidationEngineTests/ValidationTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using ValidationEngine;
+
+namespace ValidationEngineTests
+{
+    [TestFixture]
+    public class ValidationTests
+    {
+
+        [Test]
+        public void TrueForValidAddress()
+        {
+            var sut = new Validator();
+
+            sut.ValidateEmailAddress("validtest@email.se");
+
+            Assert.IsTrue(true);
+        }
+    }
+}
