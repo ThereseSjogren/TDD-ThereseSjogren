7e71f533f2b7c1382e3edde338aba2f091d071f3
Therese Sjögren <nike_8@hotmail.com>
2017-05-29 15:05:30 +0200

    task 3.1 done

--- TDD-Exercises/StringCalculator/Calculator.cs
+++ TDD-Exercises/StringCalculator/Calculator.cs
@@ -13,21 +13,33 @@ namespace StringCalculator
         {
             int result = 0;
             var sum = 0;
-            List<int> numbersList = new List<int>();
+            List<string> numbersList = new List<string>();
             List<char> delimetersList = new List<char>() { ','};
-            Regex regexSplit = new Regex(@"^[\d\s\n,]+$");
+            Regex regexSplit = new Regex(@"^\/{2}.?$");
             if (string.IsNullOrEmpty(numbers))
             {
                 return result;
             }
 
             //Splittar på kommatecknet och tar ut siffrorna som sparas i en array av strängar.
-            var splitnumber = numbers.Split(',');
+            //var splitnumber = numbers.Split(',');
             var newLineString = numbers.Split('\n');
+            if (regexSplit.IsMatch(newLineString.ToString()))
+            {
+                delimetersList.Add(Char.Parse(newLineString.ToString()));
+            }
 
+            foreach (var rowString in newLineString)
+            {
+                foreach (var delimiter in delimetersList)
+                {
+                    numbersList.AddRange(rowString.Split(delimiter));
+                }
+                
+            }
 
             //Går igenom array och parsa om dem till int
-            foreach (var n in splitnumber)
+            foreach (var n in numbersList)
             {
                 sum += int.Parse(n);
             }
