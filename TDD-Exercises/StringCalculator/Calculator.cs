544c2026e50ee60f72457fe85f15d0d633f2d099
Therese Sjögren <nike_8@hotmail.com>
2017-05-29 13:42:41 +0200

    Added exercise three and task 3.1 is in progress

---
+++ TDD-Exercises/StringCalculator/Calculator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace StringCalculator
+{
+    public class Calculator
+    {
+        public int Add(string numbers)
+        {
+            int result = 0;
+            var sum = 0;
+            List<int>numbersList = new List<int>();
+            List<char>delimetersList = new List<char>(){',','\n'};
+            Regex regexSplit = new Regex(@"^[\d\s\n,]+$");
+            if (string.IsNullOrEmpty(numbers))
+            {
+                return result;
+            }
+
+            //Splittar på kommatecknet och tar ut siffrorna som sparas i en array av strängar.
+            var splitnumber = numbers.Split(',');
+            var newLineString = numbers.Split('\n');
+
+            
+            //Går igenom array och parsa om dem till int
+            foreach (var n in splitnumber)
+            {
+                sum += int.Parse(n);
+            }
+            
+            return sum;
+        }
+    }
+}
---
+++ TDD-Exercises/StringCalculatorTests/CalculatorTests.cs
@@ -0,0 +1,56 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework.Internal;
+using StringCalculator;
+
+namespace StringCalculatorTests
+{
+    [TestFixture]
+    [Category("ExerciseThree")]
+    public class CalculatorTests
+    {
+        private Calculator sut;
+
+        [SetUp]
+        public void Setup()
+        {
+            sut = new Calculator();
+        }
+
+        [Test]
+        public void AddEmptyStringReturnZero()
+        {
+            var result = sut.Add("");
+
+            Assert.AreEqual(0,result);
+        }
+
+        [Test]
+        public void AddOneNumberReturnTheSumOfTheNumber()
+        {
+            var result = sut.Add("1");
+
+            Assert.AreEqual(1,result);
+        }
+
+        [Test]
+        public void AddTwoDifferentNumbersReturnTheSumOfTheNumbers()
+        {
+            var result = sut.Add("1,11");
+
+            Assert.AreEqual(12,result);
+        }
+
+        [Test]
+        public void AddNumberThatIsSplitOnNewLine()
+        {
+            var result = sut.Add("1\n2,3");
+
+            Assert.AreEqual(6,result);
+        }
+    }
+}
