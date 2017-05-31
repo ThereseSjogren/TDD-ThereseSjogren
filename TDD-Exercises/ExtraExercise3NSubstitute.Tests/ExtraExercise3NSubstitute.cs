2059942593a938a18a031a35fed7a6117d3576ed
Therese Sjögren <nike_8@hotmail.com>
2017-05-31 12:02:04 +0200

    Added extra exercise3 NSubstitute

---
+++ TDD-Exercises/ExtraExercise3NSubstitute.Tests/ExtraExercise3NSubstitute.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace ExtraExercise3NSubstitute.Tests
+{
+    [TestFixture]
+    [Category("ExerciseNSubstitute")]
+    public class ExtraExercise3NSubstitute
+    {
+
+
+        [Test]
+        public void CanCreateBankAccount()
+        {
+            
+        }
+        [Test]
+        public void CanNotCreateDuplicateAccounts()
+        {
+
+        }
+        [Test]
+        public void WhenCreatingAnAccount_AMessageIsWrittenToTheAuditLog()
+        {
+
+        }
+        [Test]
+        public void WhenCreatingAnValidAccount_OneMessageAreWrittenToTheAuditLog()
+        {
+
+        }
+        [Test]
+        public void WhenCreatingAnInvalidAccount_TwoMessagesAreWrittenToTheAuditLog()
+        {
+
+        }
+        [Test]
+        public void WhenCreatingAnInvalidAccount_AWarn12AndErro45MessageIsWrittenToAuditLog()
+        {
+
+        }
+        [Test]
+        public void VerifyThat_GetAuditLog_GetsTheLogFromTheAuditLogger()
+        {
+
+        }
+    }
+}
