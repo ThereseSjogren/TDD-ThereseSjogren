c7f2e74be4ff568d866b49cab0e4884d5b5ffb64
Therese Sjögren <nike_8@hotmail.com>
2017-05-31 10:06:39 +0200

    Added extra exercise 7 - func and delegates

---
+++ TDD-Exercises/ExtraExercise7FuncAndDelegate/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtraExercise7FuncAndDelegate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var order = new Order()
+            {
+                Name = "Widget A",
+                Price = 3.14m,
+                Quantity = 100,
+                Paid = false
+            };
+
+            List<Action<Order>> listOfActions = new List<Action<Order>>();
+
+            listOfActions.Add((o) => o.Name += " -In Stock");
+            listOfActions.Add((o) => o.Price = o.Price * 1.25M);
+            listOfActions.Add((o) => o.Quantity = o.Quantity - 10);
+            listOfActions.Add((o) => o.Name += "-Paid");
+            listOfActions.Add((o) => o.Paid = true);
+
+            ProcessOrder(order, listOfActions);
+            Console.WriteLine(order.Name);
+            Console.WriteLine(order.Price);
+            Console.WriteLine(order.Quantity);
+
+            Console.ReadKey();
+        }
+
+        static void ProcessOrder(Order theOrder, List<Action<Order>> actions)
+        {
+            actions.ForEach(a => a(theOrder));
+        }
+
+    }
+}
